69934c9c21a067a077ed7b732c67391815026eb3
ivan <homeemail@qq.com>
2019-06-18 19:05:52 +0800

    调整配置，加入iflex

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
   </div>
 </template>
 <script>
+const hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks');
+const buggyfill = require('viewport-units-buggyfill');
+
+
 export default {
   data() {
     return {
@@ -16,5 +20,16 @@ export default {
   created() {
     console.log('App created!');
   },
+  mounted() {
+    // 修复vierport特性的在某些浏览器上的错误行为
+    buggyfill.init({
+      hacks,
+    });
+
+    console.log('app mounted');
+  },
 };
 </script>
+<style>
+
+</style>
