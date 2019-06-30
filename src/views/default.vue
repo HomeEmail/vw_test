c315ec80fc6833852ef41ead467d4b5308211a1b
star-PC\star <591694604@qq.com>
2019-07-01 01:10:03 +0800

    完善例子

--- src/views/default.vue
+++ src/views/default.vue
@@ -2,6 +2,19 @@
   <div>
     <h1>{{lighting}}</h1>
     <div class="demo"></div>
+    <div aspectratio w-188-246 class="color">
+      <div aspectratio-content>
+        元素容器宽高比例子 postcss-aspect-ratio-mini 依赖库<br/>
+        这里放你的内容
+      </div>
+    </div>
+    <div class="example">
+      postcss-write-svg插件主要用来处理移动端1px的解决方案 使用的是border-image
+    </div>
+    <div class="example-bg"> <!-- 感觉没效果 使用方式不对？ -->
+      <div style="">postcss-write-svg插件主要用来处理移动端1px的解决方案 使用的是background-image<br/>
+      由于有一些低端机对border-image支持度不够友好，个人建议你使用background-image的这个方案。</div>
+    </div>
   </div>
 </template>
 
@@ -20,5 +33,21 @@ export default {
   width: 100px;
   height: 300px;
   background-color: red;
+}
+// 元素容器宽高比例子 postcss-aspect-ratio-mini 依赖库
+[w-188-246] {
+  aspect-ratio: '188:246'; // aspect-ratio属性不能和其他属性写在一起
+}
+[w-188-246] {
+  width: 188px;
+  background-color: yellow;
+}
+
+.example {
+  border: 1px solid transparent; // 00b1ff 是颜色值
+  border-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' height='2px'%3E%3Crect fill='%2300b1ff' width='100%25' height='50%25'/%3E%3C/svg%3E") 2 2 stretch;
+}
+.example-bg {
+  background: white url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Crect fill='%2300b1ff' width='100%25' height='100%25'/%3E%3C/svg%3E");
 }
 </style>
